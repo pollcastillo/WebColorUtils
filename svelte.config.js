5ffdca62b308a2b8c5e46e9e95d0e42028c07dd2
Poll Castillo <alejandrocordova2@icloud.com>
2024-01-25 10:03:57 -0500

    Add and configure TailwindCSS

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
+import adapter  from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
+  kit: {
+    adapter: adapter(),
+  },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
